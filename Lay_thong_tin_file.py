67862affafac1dd5d467adfa03c1be6c4942a642
NguyenCuong <88176078+vicuon@users.noreply.github.com>
2023-01-17 09:03:42 +0700

    Create Lay_thong_tin_file.py

---
+++ Lay_thong_tin_file.py
@@ -0,0 +1,20 @@
+import datetime
+import os
+import time
+
+# Đường dẫn 1 file trong thư mục
+path = r"H:\My Drive\HOCPYTHON excel\mo dau\SD\so_diem_khoi_10_mon_dia_li.xls"
+
+# lấy thông tin từ file trong thư mục ngày giờ [Cách 1: Dùng hàm datetime]
+m_time = os.path.getatime(path)
+
+# đổi thông tin ra ngày giờ
+dt_m = datetime.datetime.fromtimestamp(m_time).strftime("%d/%m/%Y, %H:%M:%S")
+print(dt_m)
+
+# lấy thời gian chỉnh sửa lần cuối cùng [cách 2: dùng hàm os và time]
+lan_cuoi = os.path.getatime(path)
+print(lan_cuoi)
+# chuyển đổi thông tin ngày giờ
+thoigian_lan_cuoi = time.strftime("%d/%m/%Y, %H:%M:%S", time.localtime(lan_cuoi))
+print(thoigian_lan_cuoi)
