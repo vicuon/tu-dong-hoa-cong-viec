735e70859c6553350ad052906a2264bfb8f54303
NguyenCuong <88176078+vicuon@users.noreply.github.com>
2023-01-22 21:32:16 +0700

    Update lay_thong_tin_04.py

--- lay_thong_tin_04.py
+++ lay_thong_tin_04.py
@@ -3,66 +3,69 @@
 # Tạo vòng lập lấy thông tin
 
 import datetime
+from itertools import count
 import os
-import time
 from tkinter.filedialog import askdirectory
 import xlwings as xw
 
-# sheet hiện hành
 sht = xw.sheets.active
 
-# lệnh hiện lên cửa sổ chọn thư mục. Lấy đườn dẫn thư mục gán vào biến
 path = askdirectory(title="Chọn thư mục")
-
-# lấy đường dẫn đến thư mục hiện hành để làm việc
 os.chdir(path)
 
-# lấy đường dẫn hiện hành, làm thư mục hiện hành làm việc
-thumuc_hienhanh = os.getcwd()
+#đường dẫn hiện hành
+dd_hienhanh = os.getcwd()
 
-#lấy tất cả tên file trong thư mục cho vào biến list
+# lấy danh sách file trong thư mục hiện hành cho vào list
 list_file = os.listdir()
 
-# đưa giá trị vào ô A3
-sht.range("A3").value = "Thư mục:" + thumuc_hienhanh
+# ghi giá trị vào ô A3 của sheet hiện hành đang mở
+sht.range("A3").value = "Thư mục chứa file: " + dd_hienhanh
 
-# số lượng file trong thư mục vào ô A4
-sht.range("A4").value = "Số file:" + str(len(list_file))
-row = 7
+# ghi số lượng file trong thư mục vào ô A4
+sht.range("A4").value = "Số file trong thư mục:" + str(len(list_file))
+
+sht.range("A6").value = "Tên file"
+sht.range("B6").value = "Thư mục chứa file"
+sht.range("C6").value = "Thời gian chỉnh sửa file"
+sht.range("D6").value = "Thời gian chỉnh sửa file lần cuối"
+sht.range("E6").value = "Thời gian khởi tạo file"
+sht.range("F6").value = "Dung lượng file (KB)"
+sht.range("G6").value = "Mở xem file"
+sht.range("H6").value = "Tên file cần chỉnh sửa"
+
+
+dong = 7
+# Tạo vòng lập lấy thông số các file trong thư mục
 for name_file in list_file:
-    # lấy đường dẫn đến file cần lấy thông tin
-    path = os.path.join(thumuc_hienhanh,name_file)
+    # ghép đường dẫn hiện hành với từng tên file trong thư mục
+    path_2 = os.path.join(dd_hienhanh,name_file)
 
-    # dấu thời gian sửa đổi tập tin của một tập tin
-    m_time = os.path.getmtime(path)  # ra một dãy số cần chuyển đổi dãy số này
-    # ngày chỉnh sửa cuối cùng
+    # thời gian sửa đổi file
+    m_time = os.path.getmtime(path_2)
     dt_m = datetime.datetime.fromtimestamp(m_time).strftime("%d/%m/%Y - %H:%M:%S")
 
+    # thời gian truy cập file cuối cùng
+    a_time = os.path.getatime(path_2)
+    dt_a = datetime.datetime.fromtimestamp(a_time).strftime("%d/%m/%Y - %H:%M:%S")
 
-    # lấy thời gian truy cập cuối cùng của tập tin
-    a_time = os.path.getatime(path)  # ra một dãy số cần chuyển đổi để lấy thông tin
-    # chuyển đổi thông tin ngày giờ
-    dt_a = time.strftime("%d/%m/%Y - %H:%M:%S", time.localtime(a_time))
+    # thời gian khởi tạo tập tin
+    c_time = os.path.getctime(path_2)
+    dt_c = datetime.datetime.fromtimestamp(c_time).strftime("%d/%m/%Y - %H:%M:%S")
 
-    # ngày giờ tạo tập tin
-    c_time = os.path.getctime(path)
-    # chuyển đổi định dạng ngày tạo file
-    dt_c = time.strftime("%d/%m/%Y - %H:%M:%S", time.localtime(c_time))
+    # lấy dung lượng file KB
+    size = os.path.getsize(path_2)
+    size_file = round(size/1024,2)
 
+    # đưa các giá trị thông tin file vào các ô
+    sht.range("A" + str(dong)).value = name_file
+    sht.range("B" + str(dong)).value = dd_hienhanh
+    sht.range("C" + str(dong)).value = dt_m
+    sht.range("D" + str(dong)).value = dt_a
+    sht.range("E" + str(dong)).value = dt_c
+    sht.range("F" + str(dong)).value = size_file
+    sht.range("G" + str(dong)).value = "=HYPERLINK({0},{1})".format('"'+path_2+'"','"Mở xem file"')
+    sht.range("H" + str(dong)).value = name_file
 
+    dong = dong + 1
 
-    # Lấy thông dung lượng file MB ()
-    size = os.path.getsize(path)
-    size_new = round(size / 1024, 2)
-
-
-    sht.range("A" + str(row)).value = name_file
-    sht.range("b" + str(row)).value = thumuc_hienhanh
-    sht.range("d" + str(row)).value = dt_m
-    sht.range("e" + str(row)).value = dt_a
-    sht.range("f" + str(row)).value = dt_c
-    sht.range("g" + str(row)).value = size_new
-    sht.range("H" + str(row)).value = "=HYPERLINK({0},{1})".format('"'+ path + '"','"Open"')
-    sht.range("I" + str(row)).value = name_file
-
-    row = row+1
